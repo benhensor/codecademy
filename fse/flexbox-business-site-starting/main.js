70bdb5536b3a5fa16fed6e12ac6e1e264734f369
benhensor <benhensor@gmail.com>
2024-04-24 12:41:10 +0100

    business site project created and added

--- fse/flexbox-business-site-starting/main.js
+++ fse/flexbox-business-site-starting/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
   $(".faq-question").click(function() {
-    $(this).next(".faq-answer").slideToggle();
-    $(this).toggleClass("active");
+    var $arrow = $(this).find(".faq-arrow"); // Find the arrow image within the clicked question
+    var $answer = $(this).next(".faq-answer");
+
+    // Slide toggle the answer and then check its visibility to toggle the arrow
+    $answer.slideToggle(400, function() {
+      // After the toggle animation completes, check if the answer is now visible
+      if ($answer.is(":visible")) {
+        $arrow.attr("src", "./assets/icons/chevron-up.svg"); // Change to up arrow when visible
+        $arrow.attr("alt", "Collapse");
+      } else {
+        $arrow.attr("src", "./assets/icons/chevron-down.svg"); // Change to down arrow when hidden
+        $arrow.attr("alt", "Expand");
+      }
+    });
   });
 });
